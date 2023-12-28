cc619c408b4416c30f1d0a4a1791964ba26b477e
Dhirajsharma2060 <Dhirajsharma2060@gmail.com>
2023-12-28 15:31:18 +0530

    hello

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from  sqlalchemy import Column,Integer,String,Boolean
+from  sqlalchemy import Column,Integer,String,Boolean,DateTime
 from .database import Base
+from datetime import datetime
 class Post(Base):
     __tablename__="post"
 
@@ -8,5 +9,5 @@ class Post(Base):
     title=Column(String,nullable=False)
     content=Column(String,nullable=False)
     published=Column(Boolean,default=True)
-    time_published=Column(default=True)
+    time = Column(DateTime, default=datetime.utcnow, nullable=False)
     
