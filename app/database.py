06acb268f92f35e221e9bdcf50841f7305938f90
Dhirajsharma2060 <Dhirajsharma2060@gmail.com>
2024-01-11 11:52:56 +0530

    hello

--- app/database.py
+++ app/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Dhiraj%402060@localhost/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,34 +1,28 @@
-from random import randrange
-import time #is ka use hota hai to create randrange ko use karne ke liye 
-from typing import Optional #pyandantic me optional ka use karne ke liye 
-from fastapi import FastAPI, HTTPException,Response,status
-from fastapi.params import Body
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from fastapi import Depends, FastAPI, HTTPException,Response,status
 from dotenv import load_dotenv
-import os
-from pydantic import BaseModel, Field
-app = FastAPI()
-
+from pydantic import BaseModel
+from .database import SessionLocal, engine
+from . import models
+from sqlalchemy.orm import Session
+from .test import test_conn
+from .connection import connect
+models.Base.metadata.create_all(bind=engine)
 load_dotenv()
-
-
+app = FastAPI()
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 class Post(BaseModel):
     title:str
     content:str
     published:bool=True
 #jab bhi test karna ho to try and except use karna 
-while True:
-    try:
-        conn=psycopg2.connect(host='localhost',database='fastapi' ,user='postgres',password= os.getenv("PASSWORD"),cursor_factory=RealDictCursor)
-        cursor=conn.cursor()
-        print("successfully connected to database")
-        break
-    except Exception as error: 
-        print("failed to connect with database") 
-        print("Error:",error) 
-        time.sleep(2)    
-    
+test_conn()
+conn, cursor = connect()
 #instead using an database i have created an my_data array which will have data stored in memory 
 my_data=[{"title":"Hello world","content":"hi","published":True,"rating":4,"id":1},
          {"title":"Hello!!!","content":"what","published":True,"rating":3,"id":4},
@@ -39,6 +33,8 @@ def find_data(id): #note here we will take the data using the id as argument
             print(f"Debug: Found post {p} at index {i}")
             return p,i
     return None,None    
+
+
 def delete_post_id(id):
     for i, p in enumerate(my_data):
         if p["id"]==id:
@@ -49,66 +45,77 @@ def delete_post_id(id):
 async def root():
     return {"message": "Hello World"}
 @app.get("/posts")
-async def posts():
-    cursor.execute("""SELECT id AS post , title FROM post""")
-    posts=cursor.fetchall()
+async def posts(db: Session = Depends(get_db)):
+    #cursor.execute("""SELECT id AS post , title FROM post""")
+    #posts=cursor.fetchall()
+    posts=db.query(models.Post).all()
 
     return {"data":posts}
-@app.post("/posts")
-async def create(post:Post):
-    cursor.execute("""
-        INSERT INTO post (title, content, published)
-        VALUES (%s, %s, %s)
-        RETURNING *;
-    """, (post.title, post.content, post.published))
+@app.get("/sqlalchemy")
+async def test_post(db: Session = Depends(get_db)):
+    posts=db.query(models.Post).all()
+    return{"success":posts}
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
+async def create(post:Post,db:Session =Depends(get_db)):
+    #cursor.execute("""
+    #    INSERT INTO post (title, content, published)
+     #   VALUES (%s, %s, %s)
+      #  RETURNING *;
+    #""", (post.title, post.content, post.published))
 
-    new_post = cursor.fetchone()
 
-    # Commit the changes to the database
-    conn.commit()
+    new_post=models.Post(title=post.title,content=post.content,published=post.published)
+    db.add(new_post)
+
+    db.commit()
+    db.refresh(new_post)
 
     return {"data" : new_post} 
 @app.get("/posts/{id}")
-async def get_post(id:str,response:Response):
-    print(type(id))#here we will print  the i
-    cursor.execute("""
-                   SELECT * from post WHERE id=%s
+async def get_post(id:str,response:Response,db:Session =Depends(get_db)):
+    #print(type(id))#here we will print  the i
+    #cursor.execute("""
+     #              SELECT * from post WHERE id=%s
 
-                   """,(str(id)))
-    post=cursor.fetchone()
+      #             """,(str(id)))
+    #post=cursor.fetchone()
+    post=db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"the id {id} does not exist")
         #note aha pe deatil hoga detais nahi 
     return {"post_detail":post}#yaha pe vho post ko call karega post ko jho find_data ka use karega 
    # return{"post details":posts}
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)#note posts hai waha pe post nahi wording pe dhiyan rakhna 
-async def delete_post(id:int):
-    cursor.execute("""
-                    DELETE FROM post WHERE id=%s  returning*
-                   """,(str(id)),)
-    delete_post_Id=cursor.fetchone()
-    conn.commit()
-    if delete_post_Id is None:
-        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,
-                            detail=f"post with id :{id} does not exist ")
-    
+async def delete_post(id:int,db:Session =Depends(get_db)):
+    #cursor.execute("""
+     #               DELETE FROM post WHERE id=%s  returning*
+     #              """,(str(id)),)
+    #delete_post_Id=cursor.fetchone()
+    #conn.commit()
+    #delete_post_Id=db.query(models.Post.id==id)
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist")
+    post.delete(synchronize_session=False) 
+    db.commit()   
     return Response(status_code=status.HTTP_204_NO_CONTENT)#note reponse type bhejna hai to {} nahi karna hai 
 
 @app.put("/posts/{id}")
-async def update_post_id(id: int, post: Post):
-    cursor.execute("""
-                    UPDATE post SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
-                    (post.title,post.content,post.published,str(id)))
-    update_post_Id=cursor.fetchone()
-    conn.commit()
-   
-    
-    if update_post_Id is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
-
-    
-    
-    return {"data": update_post_Id}
+async def post_query_id(id: int, updated_post: Post,db:Session =Depends(get_db)):
+    #cursor.execute("""
+     #               UPDATE post SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
+      #              (post.title,post.content,post.published,str(id)))
+    #post_query_Id=cursor.fetchone()
+    post_query=db.query(models.Post).filter(models.Post.id == id)
+    post=post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id : {id} does not exist")
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()} 
 
 
 
